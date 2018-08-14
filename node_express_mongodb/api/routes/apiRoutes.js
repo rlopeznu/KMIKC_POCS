999c9a7c6d0f9dbebb78219392af9c57f813254c
Marcela Guzman <mguzmama@everis.com>
2018-08-14 16:42:13 -0300

    se cambia headers

--- node_express_mongodb/api/routes/apiRoutes.js
+++ node_express_mongodb/api/routes/apiRoutes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 
     app.all('/*', function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
     });
 
